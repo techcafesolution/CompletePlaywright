77ad76119c1e832f0dcae7eb63788a86b5efcd65
Sanjeeb.dash <sanjeebavana@gamial.com>
2025-01-02 10:33:15 +0530

    added Locator class

---
+++ tests/Locators.spec.js
@@ -0,0 +1,25 @@
+const{test,aspect}=require('@playwright/test');
+
+test('LocatorsTest',async ({page})=>{
+
+await page.goto("https://testautomationpractice.blogspot.com/");
+
+//1. How to fill value.
+// 1st way by fill()
+await page.locator('#name').fill("Sanjeeb");
+//2nd way by type()--> Depricated no more use.
+await page.locator('#email').type("sanjeebavana@gmail.com");
+//3rd way without using locator method
+await page.fill('#phone', "865974556");
+//2. How to perform click operation.
+//1st way of click() by using locator.
+await page.locator("//input[@id='male']").click();
+//2nd way of click() without using locator.
+await page.click("//input[@id='female']");
+//3. How to capture multiple web-elements
+const locators=await page.$$("//select[@id='country']//option");
+for(const locator of locators){
+    let option=await locator.textContent();
+    console.log(option);
+};
+});
